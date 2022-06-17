f6ac47e83cc2efc2cd211e2933e6cef7ff9cf380
Relrige <91017454+Relrige@users.noreply.github.com>
2022-06-17 19:34:45 +0300

    Add files via upload

---
+++ dir/New+CW/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+namespace New_CW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string path;
+            //Console.WriteLine("enter folder path");
+            //path = Console.ReadLine();
+            //PrintFolder(path);
+
+
+            Directory.SetCurrentDirectory(@"E:\");
+            Directory.CreateDirectory("TEST");
+            Directory.SetCurrentDirectory(@"E:\TEST");
+            Directory.CreateDirectory("A");
+            Directory.CreateDirectory("B");
+            string fname = "first.txt";
+            Console.WriteLine("\n\t\tEnter text  : ");
+            File.WriteAllText(fname, Console.ReadLine());
+            string str1 = @"A\firstA.txt";
+            if (File.Exists(str1))
+            {
+                Console.WriteLine($"File  already  exist");
+            }
+            else
+                File.Copy(fname, str1);
+
+
+            string str2 = @"B\firstB.txt";
+            if (File.Exists(str2))
+            {
+                Console.WriteLine($"File  already  exist");
+            }
+            else
+                File.Copy(fname, str2);
+
+
+
+            Console.WriteLine("____A___");
+            Console.WriteLine(File.ReadAllText(str1));
+            FileInfo fi = new FileInfo(str1);
+            Console.WriteLine($"Creation time :{fi.CreationTime}");
+            Console.WriteLine($"Lenght :{fi.Length}");
+
+
+            Console.WriteLine("____B___");
+            Console.WriteLine(File.ReadAllText(str2));
+            FileInfo fi1 = new FileInfo(str2);
+            Console.WriteLine($"Creation time :{fi1.CreationTime}");
+            Console.WriteLine($"Lenght :{fi1.Length}");
+        }
+        static void PrintFolder(string path)
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+            if (!di.Exists)
+            {
+                Console.WriteLine($"Error : folder {path} not found");
+                return;
+            }
+            foreach (FileInfo f in di.EnumerateFiles())
+            {
+                string info = "<DIR>";
+                if (f.Attributes.HasFlag(FileAttributes.Directory))
+                    info = (f as FileInfo)?.Length.ToString()??"";
+                Console.WriteLine($"{f.Name,-40} {f.CreationTime,-20} {info}");
+            }
+        }
+    }
+}
---
+++ dir/dir/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System;
+using System.Collections.Generic;
+
+namespace dir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine($"Current path :{Directory.GetCurrentDirectory()}");
+            
+            string dirPath = "VPD 121";
+            if(Directory.Exists(dirPath))
+            {
+                Console.WriteLine("Exists");
+            }
+            else
+            {
+                DirectoryInfo dir1 = Directory.CreateDirectory(dirPath);
+                Console.WriteLine(dir1.Exists);
+            }
+            string fname;
+            Console.WriteLine("Enter name of file");
+            fname=Console.ReadLine();
+            string pathFname = Path.Combine(dirPath, fname);
+            Console.WriteLine($"Will be created file {pathFname}");
+
+            Console.WriteLine("Enter content ");
+            File.WriteAllText(pathFname, Console.ReadLine());
+
+            List<string> lines = new List<string> { "]n C# Files", "class fsfasasf" };
+            File.AppendAllLines(pathFname, lines);
+
+            FileInfo fi = new FileInfo(pathFname);
+        }
+    }
+}
